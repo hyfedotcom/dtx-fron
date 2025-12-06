d9260b586a8043d97ea2157afb5acbc829f62d28
StenLy <kaw(ek+ig;vug)veu%heRc&egeM>
2025-12-06 16:27:11 +0100

    add image cache,

--- next.config.ts
+++ next.config.ts
@@ -6,19 +6,36 @@ const nextConfig: NextConfig = {
       { protocol: "http", hostname: "localhost", port: "1337" },
       { protocol: "https", hostname: "*.strapiapp.com" },
     ],
-    minimumCacheTTL: 0,
+    minimumCacheTTL: 31536000,
     formats: ["image/webp", "image/avif"],
   },
 
-  // async redirects() {
-  //   return [
-  //     {
-  //       source: "/",
-  //       destination: "/home",
-  //       permanent: true,
-  //     },
-  //   ];
-  // },
+  experimental: {
+    optimizePackageImports: ["framer-motion"],
+  },
+
+  async headers() {
+    const isProd = process.env.NODE_ENV === "production";
+
+    return [
+      {
+        source: "/:path*",
+        headers: isProd
+          ? [
+              {
+                key: "Cache-Control",
+                value: "public, max-age=31536000, immutable",
+              },
+            ]
+          : [
+              {
+                key: "Cache-Control",
+                value: "no-store",
+              },
+            ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
