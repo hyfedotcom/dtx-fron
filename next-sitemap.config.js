8008d1b153b34c58a5ab7b510df2e2833bf0d9fa
StenLy <kaw(ek+ig;vug)veu%heRc&egeM>
2025-12-12 14:30:03 +0100

    activate next-sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "http://localhost:3000",
+  siteUrl: "https://www.resolvedtx.com/",
   generateRobotsTxt: true,
   sitemapSize: 5000,
 };
