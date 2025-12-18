240925bd46eb2b2ebeb8e09d6fbe9d8096b2718d
StenLy <kaw(ek+ig;vug)veu%heRc&egeM>
2025-12-18 17:18:20 +0100

    fix clarity and google

--- app/components/analytics/Google.tsx
+++ app/components/analytics/Google.tsx
@@ -2,24 +2,26 @@
 
 import Script from "next/script";
 
+const GA_ID = "G-3GD8BYW5HK";
+
 export default function Google() {
   return (
     <>
       <Script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-3GD8BYW5HK"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+        strategy="afterInteractive"
       />
-      <Script
-        id="ga-4"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-3GD8BYW5HK', { anonymize_ip: true });
-          `,
-        }}
-      />
+
+      <Script id="ga-4" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_ID}', {
+            anonymize_ip: true
+          });
+        `}
+      </Script>
     </>
   );
 }
