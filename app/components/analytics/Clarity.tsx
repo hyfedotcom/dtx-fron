29b458015151be0dcae754912c2cc4133ef9eee9
StenLy <kaw(ek+ig;vug)veu%heRc&egeM>
2025-12-18 17:11:12 +0100

    change clarity number

--- app/components/analytics/Clarity.tsx
+++ app/components/analytics/Clarity.tsx
@@ -4,11 +4,11 @@ export default function Clarity() {
   return (
     <Script id="microsoft-clarity" strategy="afterInteractive">
       {`
-        (function(c,l,a,r,i,t,y){
-          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-        })(window, document, "clarity", "script", "ulykhmcay3");
+          (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src=“https://www.clarity.ms/tag/”+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+             })(window, document, “clarity”, “script”, “ulyjwo4bax”);
       `}
     </Script>
   );
